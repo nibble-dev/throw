a82fa1a09503ad50a40385c5638c7eff8b1bd8b8
BrumaMan <bmazurowski07@gmail.com>
2025-07-16 01:50:57 +0100

    initial commit

---
+++ src/throw.h
@@ -0,0 +1,51 @@
+/*
+ * throw.h
+ *
+ * Originally written by Joseph Werle (2013) – MIT License
+ * https://github.com/jwerle/throw.h
+ *
+ * Modified in 2025 by Bartosz Mazurowski <bartosz.mazurowski@protonmail.com>
+ */
+ 
+#ifndef THROW_H
+#define THROW_H 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int code;
+    char *name;
+} error;
+
+FILE *THROW_FD = NULL;
+
+void throw(void *type, char *msg);
+
+void throw_fatal(void *type, char *msg);
+
+error error_new(int code, char *name);
+
+error error_new(int code, char *name) {
+    error err;
+    err.code = code;
+    err.name = name;
+    return err;
+}
+
+void throw(void *type, char *msg) {
+    error *err = (error *)type;
+    if (THROW_FD == NULL) {
+        THROW_FD = stderr;
+    }
+    
+    fprintf(THROW_FD, "%s [%d]: %s\n", err->name, err->code, msg);
+}
+
+void throw_fatal(void *type, char *msg) {
+    error *err = (error *)type;
+    throw(type, msg);
+    exit(err->code);
+}
+
+#endif
---
+++ test.c
@@ -0,0 +1,9 @@
+#include "src/throw.h"
+
+int main() {
+    error test = error_new(1, "test error");
+    
+    throw(&test, "This will print error to stderr or whatever THROW_FD is set to");
+
+    throw_fatal(&test, "This will exit after throwing");
+}
